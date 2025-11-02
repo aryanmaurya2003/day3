f5049b9970258d4e786d7a3d88b9ac7f40a831b9
aryanmaurya2003 <ramessharma20012@gmail.com>
2025-11-02 13:22:53 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,69 @@
+(function () {
+
+    function CardNew(name, price, category, discount = 0) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.discount = discount
+        this.getDetails = function () {
+            console.log(`Name: ${this.name}, Price: ${this.price}, Category: ${this.category}`);
+            return "a";
+        }
+        this.productCard = function () {
+            discountedPrice = this.getDiscountedPrice()
+            var card1 = document.createElement("div")
+            card1.innerHTML = `<h1>Product Name ${this.name}</h1><h2>price is ${this.price}</h2><p>Category is Mobile ${this.category}.</p><h3>Discount is ${this.discount}% and discounted  Price is ${discountedPrice}</h3>`
+            card1.classList.add("card8", "product-card")
+            document.body.appendChild(card1)
+            console.log("Product Card Method Called",this.price);
+            return {productName:this.name,price:this.price};
+        }
+    }
+
+    CardNew.prototype.getDiscountedPrice = function () {
+        console.log("Discounted Price Method Called");
+        return this.price - (this.price * this.discount / 100);
+    }
+
+    var newdata = new CardNew("lamp", 200, "electronics", 20);
+    newdata.getDetails();
+
+    newdata.getDiscountedPrice();
+    data1=newdata.productCard();
+
+    var newdata2 = new CardNew("mobile", 5000, "electronics", 30);
+    data2=newdata2.productCard();
+
+    var newdata3 = new CardNew("laptop", 50000, "electronics", 40);
+    data3=newdata3.productCard();
+
+    var newdata4 = new CardNew("jeans", 999, "electronics", 15);
+    data4=newdata4.productCard();
+
+
+
+
+    const data=[data1,data2,data3,data4];
+
+
+    console.log(data);
+ 
+
+
+
+    var element = document.getElementsByClassName("product-card");
+
+    for (var i = 0; i < element.length; i++) {
+        element[i].addEventListener("mouseover", function () {
+            this.style.transform = "scale(1.1)";
+            this.style.transition = "all 0.3s ease-in-out";
+        });
+
+        element[i].addEventListener("mouseleave", function () {
+            this.style.transform = "scale(1)";
+            this.style.transition = "all 0.3s ease-in-out";
+        });
+    }
+
+
+})();
